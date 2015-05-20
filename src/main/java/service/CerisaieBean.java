f3bf54c2a712b9808768c51bf9b6474e0229c6c3
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-05-20 16:12:30 +0200

    Reformat code

--- src/main/java/service/CerisaieBean.java
+++ src/main/java/service/CerisaieBean.java
@@ -1,15 +1,12 @@
 package service;
 
+import dao.Client;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.naming.NamingException;
-
-import dao.Client;
+import java.util.List;
 
 /**
  * Session Bean implementation class CerisaieBean
@@ -18,7 +15,7 @@ import dao.Client;
 @Remote(CerisaieBeanRemote.class)
 public class CerisaieBean implements CerisaieBeanRemote {
 
-    @PersistenceContext(unitName = "JPAMavenCerisaie")
+    @PersistenceContext(unitName = "JPACerisaie")
     private EntityManager em;
 
     public void ajouter(Client unclient) throws Exception {
@@ -58,9 +55,8 @@ public class CerisaieBean implements CerisaieBeanRemote {
 
 
     @SuppressWarnings("unchecked")
-    public List<Object>  listerTousLesSportsClient () throws Exception
-    {
-		/*Select  j.numsej , c.nomCli, s.libelleSport, a.datejour
+    public List<Object> listerTousLesSportsClient() throws Exception {
+        /*Select  j.numsej , c.nomCli, s.libelleSport, a.datejour
 		From Client c, activite a, Sport s, Sejour j
 		where c.numcli = j.numcli 
 		and a.numsej  = j.numsej
@@ -69,11 +65,11 @@ public class CerisaieBean implements CerisaieBeanRemote {
 
 
         String hql = " SELECT  j.numSej, c.nomCli, s.libelleSport, a.id.dateJour ";
-        hql+=" from Client c, Activite a, Sejour j, Sport s ";
-        hql+= " where c.numCli = j.client.numCli  ";
-        hql+= " and a.sejour.numSej  = j.numSej ";
-        hql+=" and a.sport.codeSport = s.codeSport ";
-        hql+= " order by j.numSej";
+        hql += " from Client c, Activite a, Sejour j, Sport s ";
+        hql += " where c.numCli = j.client.numCli  ";
+        hql += " and a.sejour.numSej  = j.numSej ";
+        hql += " and a.sport.codeSport = s.codeSport ";
+        hql += " order by j.numSej";
 
         try {
             return em.createQuery(hql).getResultList();
@@ -83,8 +79,6 @@ public class CerisaieBean implements CerisaieBeanRemote {
             throw ne;
         }
     }
-
-
 
 
     /**
--- src/main/java/service/CerisaieBeanRemote.java
+++ src/main/java/service/CerisaieBeanRemote.java
@@ -1,19 +1,20 @@
 package service;
 
-import javax.ejb.Remote;
-import javax.naming.NamingException;
-
-
 import dao.Client;
 
-import java.util.*;
+import javax.ejb.Remote;
+import javax.naming.NamingException;
+import java.util.List;
 
 @Remote
 public interface CerisaieBeanRemote {
 
     public void ajouter(Client c) throws NamingException, Exception;
+
     public Client rechercherClient(String id) throws NamingException, Exception;
-    public List<Client> listerTousLesClients() throws NamingException,Exception;
-    public List<Object>  listerTousLesSportsClient () throws NamingException,Exception;
+
+    public List<Client> listerTousLesClients() throws NamingException, Exception;
+
+    public List<Object> listerTousLesSportsClient() throws NamingException, Exception;
 
 }
